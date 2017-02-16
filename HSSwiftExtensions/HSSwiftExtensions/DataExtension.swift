25b16f77fdd8847be43553917707db1e32978465
Hisen <gnetop@hisendeimac.local>
2017-02-16 19:11:22 +0800

    添加UIViewController的extension

---
+++ HSSwiftExtensions/HSSwiftExtensions/DataExtension.swift
@@ -0,0 +1,15 @@
+//
+//  DataExtension.swift
+//  HSSwiftExtensions
+//
+//  Created by Hisen on 2017/2/14.
+//  Copyright © 2017年 haisheng huang. All rights reserved.
+//
+
+import Foundation
+//import CommonCrypto
+
+public extension Data {
+
+    
+}
