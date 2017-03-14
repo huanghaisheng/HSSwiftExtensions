d717a0f991192dbd16c2fc3023bc8943728a9f19
haisheng huang <haisheng.huang@hotmail.com>
2017-03-14 16:01:41 +0800

    添加demo

---
+++ HSSwiftExtensionsDemo/HSSwiftExtensionsDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HSSwiftExtensionsDemo
+//
+//  Created by haisheng huang on 2017/3/14.
+//  Copyright © 2017年 haisheng huang. All rights reserved.
+//
+
+import UIKit
+import HSSwiftExtensions
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
